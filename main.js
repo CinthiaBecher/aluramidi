e989e2e5bf364b29bc4ca9b18494c25ad8bd8274
Cinthia Becher <cinthia_becher@hotmail.com>
2022-12-19 18:58:05 -0300

    Audio Functions

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-document.querySelector('.tecla_pom');
+function tocaSom( idElementoAudio ){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento= tecla.classList[1];
+    //#som_{} - template string
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown =  function (){
+        tecla.classList.add('ativa');
+    }
+}
+
+/*
+let i = 0;
+while (i < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento= tecla.classList[1];
+
+    //#som_{} - template string
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    i = i +1;
+}*/
